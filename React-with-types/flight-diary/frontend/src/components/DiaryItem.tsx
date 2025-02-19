a2e676d1354c2851a2c3816da77860f474a15475
Jere Lehtinen <93128297+jvwlehti@users.noreply.github.com>
2025-02-19 17:18:34 +0200

    exercises 9.18-9.20

--- React-with-types/flight-diary/frontend/src/components/DiaryItem.tsx
+++ React-with-types/flight-diary/frontend/src/components/DiaryItem.tsx
@@ -8,6 +8,7 @@ export const DiaryItem = (props: Diary) =>{
             <h3>{diary.date}</h3>
             <p>visibility: {diary.visibility}</p>
             <p>weather: {diary.weather}</p>
+            {diary.comment && <p>comment: {diary.comment}</p>}
         </div>
     )
 }
